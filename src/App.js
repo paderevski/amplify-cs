d973707aa5a10a4afea1d066797bf88495e0ce3b
Patrick White <pwhitetj@users.noreply.github.com>
2023-08-10 12:02:35 -0400

    new version of node

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Learn CS. Online.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
